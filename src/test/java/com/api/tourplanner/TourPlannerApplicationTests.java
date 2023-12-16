bbf6c5e70f972bdfca2d9499caf0d8061fc6b671
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2023-12-16 21:38:48 +0530

    Added SpringBootTest for the integration testing

--- src/test/java/com/api/tourplanner/TourPlannerApplicationTests.java
+++ src/test/java/com/api/tourplanner/TourPlannerApplicationTests.java
@@ -1,13 +1,36 @@
 package com.api.tourplanner;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TourPlannerApplicationTests {
 
+	@Autowired
+	TestRestTemplate testRestTemplate;
+
 	@Test
-	void contextLoads() {
+	void shouldReturnAllJourneyInformations() {
+		ResponseEntity<String> response =  testRestTemplate.getForEntity("/journey/details", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	@Test
+	void shoouldReturnSingleJourneyInformationBasedOnId() {
+		ResponseEntity<String> response = testRestTemplate.getForEntity("/journey/detail/1", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		DocumentContext responseContent = JsonPath.parse(response.getBody());
+		Number id = responseContent.read("$.id");
+		assertThat(id).isNotNull();
+		assertThat(id).isEqualTo(1);
 	}
 
 }
